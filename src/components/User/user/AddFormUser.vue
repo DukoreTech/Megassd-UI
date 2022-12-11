468343e31bbb9f9862bb465c640c37b014957148
CyberShriver <irakozep18.pi@gmail.com>
2022-12-11 19:11:01 +0000

    create add user form

--- src/components/User/user/AddFormUser.vue
+++ src/components/User/user/AddFormUser.vue
@@ -1,13 +1,218 @@
 <template>
-  
+<div>
+<!-- retrieve data -->
+<span class="d-none">{{$store.state.users}}{{$store.state.IdEditUser}}</span>
+<!-- retrieve data -->
+   
+
+  <div class="register">
+    <form action="" @submit.prevent="saveInformation">
+
+        <label for="nom">
+            <input type="text" id="nom" placeholder="nom" v-model="form.nom">
+            <span>Nom</span>
+        </label>
+         <span>{{ errors?.nom }}</span>
+
+         <label for="prenom">
+            <input type="text" id="prenom" placeholder="prenom" v-model="form.prenom">
+            <span>Prenom</span>
+        </label>
+         <span>{{ errors?.prenom }}</span>
+
+          <label for="dateDenaissance">
+            <input type="datetime-local" id="dateDenaissance" placeholder="Date de naissance" v-model="form.dateDenaissance">
+            <span>Date de naissance</span>
+         </label>
+         <span>{{ errors?.dateDenaissance }}</span>
+
+         <label for="telephone">
+            <input type="tel" id="telephone" placeholder="Telephone" v-model="form.telephone">
+            <span>Telephone</span>
+         </label>
+         <span>{{ errors?.telephone }}</span>
+
+         <label for="mail">
+            <input type="email" id="mail" placeholder="Telephone" v-model="form.mail">
+            <span>Email</span>
+         </label>
+         <span>{{ errors?.mail }}</span>
+
+         <label for="Adresse">
+            <input type="text" id="Adresse" placeholder="Adresse" v-model="form.adresse">
+            <span>Adresse</span>
+         </label>
+         <span>{{ errors?.adresse }}</span>
+
+        <label for="Username">
+            <input type="text" id="Username" placeholder="Username" v-model="form.Username">
+            <span>Username</span>
+        </label>
+        <span>{{ errors?.Username }}</span>
+
+         <label for="motDePasse">
+            <input type="password" id="motDePasse" placeholder="Mot de passe" v-model="form.motDePasse">
+            <span>Mot de passe</span>
+        </label>
+         <span>{{ errors?.motDePasse }}</span>
+
+<!-- 
+        <label for="usr">
+            <input type="text" id="user" placeholder="User" v-model="form.user_id">
+            <span>User</span>
+        </label>
+         <span>{{ errors?.user_id }}</span> -->
+
+        <!-- <button type="button">Register</button> -->
+        <button type="submit" class="btn btn-sm btn-danger float-end" >{{saveEditBtn}}</button>
+    </form>
+</div>
+
+</div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
+  props:["modalActive"],
+  data() {
+    return {
+      form: {
+        nom:"",
+        prenom:"",
+        dateDenaissance:"",
+        telephone:"",
+        mail:"",
+        adresse:"",
+        Username:"",
+        motDePasse:""      
+      },
+      errors: {},
+      users:[],
+      saveEditBtn:"Enregistrer",
+    };
+  },
 
+  updated(){
+    if(this.$store.state.IdEditUser==null)return;
+    this.form=this.$store.state.users;
+    this.saveEditBtn="Modifier"
+  },
+
+  methods: {
+ 
+    saveInformation() {
+      if (this.form["Username", "motDePasse", "mail", "nom"]=="") return; 
+
+       if(this.$store.state.IdEditUser==null){
+             
+        axios.post(
+          this.$store.state.baseUrl + "/users",
+          this.form
+        )
+        .then((resp) => {
+          this.users = resp.data;
+          this.form = { nom:"",prenom:"",dateDenaissance:"", telephone:"",mail:"",adresse:"",Username:"",motDePasse:""} 
+        })
+        .catch((err) => {
+          console.error(err.response.data.errors);
+          this.errors = err.response.data.errors;
+        });
+       }else{
+         axios.patch(
+          this.$store.state.baseUrl+"/users/"+this.$store.state.IdEditUser,
+          this.form )
+        .then((resp) => {
+          this.users = resp.data;
+          this.$emit('close')
+         })
+        .catch((err) => {
+          console.error(err.response.data.errors);
+          this.errors = err.response.data.errors;
+        });
+
+       }
+ 
+    }
+    
+}
 }
 </script>
 
-<style>
+<style  scoped>
+
+  *{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+}
+body{
+    min-height:100vh;
+    display:grid;
+    place-content:center;
+    font-family:sans-serif;
+    color:#6b6b6b;
+ 
+}
+form{
+    width:90vw;
+    max-width:768px;
+    /* border:1px solid #ddd; */
+    padding:3vw;
+    display:flex;
+    flex-direction:column;
+    border-radius:5px;
+}
+
+label{
+    margin-bottom:15px;
+    position:relative;
+    border-bottom:1px solid #ddd;
+}
+input,select{
+    width:100%;
+    padding:10px 0px;
+    margin-top:20px;
+    border:none;
+    outline:none;
+}
+input::placeholder{
+    opacity:0;
+}
+
+select::placeholder{
+    opacity:0;
+}
+label span{
+    position:absolute;
+    top:0;
+    left:0;
+    transform:translateY(30px);
+    font-size:0.825em;
+    transition-duration:300ms;
+}
+ span{
+    position:relative;
+    bottom:10;
+    left:0;
+    transform:translateY(10px);
+    font-size:0.825em;
+}
+button{
+    padding:15px 0px; 
+    margin-top:20px;
+    background:rgb(75, 126, 160);
+    color:#fff;
+    border:1px solid purple;
+    cursor:pointer;
+    border-radius:3px;
+    width: 100px;
+    float:right;
+}
+label:focus-within > span,
+input:not(:placeholder-shown) + span{
+    color:purple;
+    transform:translateY(0px);
+}
 
 </style>
