d6bf6d4439c829a47e5f31c98b35fcddbd70bb05
CyberShriver <irakozep18.pi@gmail.com>
2022-12-11 19:37:42 +0000

    cerate UserView file in view folder

---
+++ src/views/UserView.vue
@@ -0,0 +1,16 @@
+<template>
+    <UserList/>
+</template>
+<script>
+export default {
+
+    components: {
+        'UserList': require("@/components/User/user/ListUsers.vue").default,
+    }
+
+}
+</script>
+
+<style>
+
+</style>
