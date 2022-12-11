913b8495e303b64f2e8db5a3d9adc7073daeee64
CyberShriver <irakozep18.pi@gmail.com>
2022-12-11 17:55:00 +0000

    import navbar components in app.vue core

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <template>
-   <router-view/>
+   <div class="col-md-12">
+         <navbar/>
+      <span class="d-flex">
+         <div class="col-md-2 border" id="sideMenu" >
+         </div>
+         <div class="col-md-10">
+            <div class="my-3">
+               <router-view/>
+            </div> 
+            <div class="fixed-bottom">
+            </div>
+         </div>
+      </span>
+
+  </div>
+
 </template>
 
 <script>
 export default {
+   components:{
+      'navbar':require("@/components/Dashboard/NavBar.vue").default
+   }
 
 }
 </script>
