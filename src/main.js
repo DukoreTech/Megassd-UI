ffadc999933ac957c682c00bb12cbbfecb1f7e4d
CyberShriver <irakozep18.pi@gmail.com>
2022-12-11 17:34:50 +0000

    first commit

---
+++ src/main.js
@@ -0,0 +1,29 @@
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret,faUser,faTachometerAlt,faHouse,faShoppingCart,faCartPlus,faCog,faPlus,faPlusCircle,faEdit,faUserEdit,faTrash,faTrashAlt,faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret,faUser,faTachometerAlt,faHouse,faShoppingCart,faCartPlus,faCog,faPlus,faPlusCircle,faEdit,faUserEdit,faTrash,faTrashAlt,faSignOutAlt)
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+// import "@/assets/vendor/datatables-demo.js"
+// import "@/assets/vendor/datatables/dataTables.bootstrap4.js"
+// import "@/assets/vendor/datatables/jquery.dataTables.js"
+
+createApp(App)
+    .use(store)
+    .use(router)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+
+module.exports={chainWebpack:
+config=>{
+  config.plugin('html').tap(args=>{
+    args[0].title='Megassd-UI';
+    return args;
+  });
+}}
