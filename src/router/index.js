df4613dec3a99d54738fdd92f9bbc89b64c453a3
CyberShriver <irakozep18.pi@gmail.com>
2022-12-12 08:47:23 +0000

    add client url in router

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import User from "@/views/UserView.vue"
 import Role from "@/views/RoleView.vue"
 import Adresse from "@/views/AdresseView.vue"
-import Adresse from "@/views/AdresseView.vue"
+import Client from "@/views/ClientView.vue"
 
 const routes = [
 
  { path:'/User',name:'User',component:User },
  { path:'/Role',name:'Role',component:Role },
  { path:'/Adresse',name:'Adresse',component:Adresse },
- { path:'/Adresse',name:'Adresse',component:Adresse },
+ { path:'/Client',name:'Client',component:Client },
  
 ]
 
