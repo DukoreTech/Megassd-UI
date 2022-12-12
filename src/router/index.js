cbce9cbc4a55eb4192be33bb1c55e338516db16d
CyberShriver <irakozep18.pi@gmail.com>
2022-12-12 08:20:48 +0000

    add url and component in router for adresse

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import User from "@/views/UserView.vue"
 import Role from "@/views/RoleView.vue"
+import Adresse from "@/views/AdresseView.vue"
 
 const routes = [
 
  { path:'/User',name:'User',component:User },
  { path:'/Role',name:'Role',component:Role },
+ { path:'/Adresse',name:'Adresse',component:Adresse },
  
 ]
 
