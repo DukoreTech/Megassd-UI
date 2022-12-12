2e12261723671b7f86be1f58f7b51275e0b63b62
CyberShriver <irakozep18.pi@gmail.com>
2022-12-12 09:32:52 +0000

    create type client view file

---
+++ src/views/TypeClientView.vue
@@ -0,0 +1,16 @@
+<template>
+    <TypeClientList/>
+</template>
+<script>
+export default {
+
+    components: {
+        'TypeClientList': require("@/views/TypeClientView").default,
+    }
+
+}
+</script>
+
+<style>
+
+</style>
