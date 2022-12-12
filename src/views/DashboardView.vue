29c9f2caee28f1841d5bacab70aa06e4f85d9772
CyberShriver <irakozep18.pi@gmail.com>
2022-12-12 12:40:33 +0000

    create dashboard view file

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,16 @@
+<template>
+    <DashBoard/>
+</template>
+<script>
+export default {
+
+    components: {
+        'DashBoard': require("@/components/Dashboard/DashBoard").default,
+    }
+
+}
+</script>
+
+<style>
+
+</style>
