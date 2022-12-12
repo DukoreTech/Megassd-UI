76f71511f197fbf3ec804981114c438b0b407941
CyberShriver <irakozep18.pi@gmail.com>
2022-12-12 11:01:32 +0000

    create dtock view file

---
+++ src/views/StockView.vue
@@ -0,0 +1,16 @@
+<template>
+    <StockList/>
+</template>
+<script>
+export default {
+
+    components: {
+        'StockList': require("@/components/Stock/Stock/StockList").default,
+    }
+
+}
+</script>
+
+<style>
+
+</style>
