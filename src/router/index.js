56d8d4f088eab2a1d126374ab8faa474c7e48f9c
CyberShriver <irakozep18.pi@gmail.com>
2022-12-12 09:35:00 +0000

    add tyep client url and component in router

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import User from "@/views/UserView.vue"
 import Role from "@/views/RoleView.vue"
 import Adresse from "@/views/AdresseView.vue"
 import Client from "@/views/ClientView.vue"
+import TyClient from "@/views/TypeClientView.vue"
 
 const routes = [
 
@@ -10,6 +11,7 @@ const routes = [
  { path:'/Role',name:'Role',component:Role },
  { path:'/Adresse',name:'Adresse',component:Adresse },
  { path:'/Client',name:'Client',component:Client },
+ { path:'/TyClient',name:'TyClient',component:TyClient },
  
 ]
 
