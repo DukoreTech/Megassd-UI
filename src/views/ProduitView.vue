72740cca05a3488dd8bf5570c592cd15697bbd77
CyberShriver <irakozep18.pi@gmail.com>
2022-12-12 10:29:12 +0000

    cerate produit view

---
+++ src/views/ProduitView.vue
@@ -0,0 +1,16 @@
+<template>
+    <ProduitList/>
+</template>
+<script>
+export default {
+
+    components: {
+        'ProduitList': require("@/components/Stock/Produit/ProduitList").default,
+    }
+
+}
+</script>
+
+<style>
+
+</style>
