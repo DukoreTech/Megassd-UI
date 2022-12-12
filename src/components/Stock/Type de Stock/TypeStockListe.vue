00604e0a8e0599f6d7aa58e0f69d42a74ca936f5
CyberShriver <irakozep18.pi@gmail.com>
2022-12-12 11:13:50 +0000

    create stock list compt

---
+++ src/components/Stock/Type de Stock/TypeStockListe.vue
@@ -0,0 +1,111 @@
+<template>
+<div>
+        <div>        
+                <button class="btn btn-info m-5" @click="modalActive = true,$store.state.IdEditTypStock=null">
+                    <font-awesome-icon icon="fa-solid fa-plus-circle" />
+                    Ajouter type de Client
+                  </button>
+                <div class="m-3 float-end ">
+                        <input type="text" class="form-control"  v-model="search" placeholder="Search" @keypress.enter="searchEvery"/>
+                    </div>
+                <modal-component :modalActive="modalActive" @close="modalActive = !modalActive ">
+                    <add-form @close="modalActive = !modalActive"/>
+                </modal-component>
+        </div>
+
+         <div class="container-fluid">
+            <div class="card  mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-mute">Liste des type des stock</h6>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered  table-striped table-hover text-center" id="dataTable" width="100%" cellspacing="0">
+                            <thead>
+                              <tr>                    
+                                <th scope="col">Id</th>
+                                <th scope="col">Type de stock</th>
+                                <th scope="col">Description</th>                           
+                                <th scope="col">Actions</th>
+                             </tr>
+                            </thead>
+                           
+                            <tbody>
+                               <tr v-for="typeStock in typeStocks" :key="typeStock.id">
+                                <th scope="row">{{ typeStock.id }}</th>
+                                <td>{{ typeStock.typeStock }} </td>
+                                <td>{{ typeStock.description }} </td>         
+                                <td>
+                                    <button class="btn btn-sm btn-default m-2"  @click="deleteTStock(typeStock.id)"><font-awesome-icon icon="fa-solid fa-trash"/>
+                                    </button>
+                                    <button class="btn btn-sm btn-default" @click="modalActive = true,editTypeStock(typeStock,typeStock.id)" >
+                                    <font-awesome-icon icon="fa-solid fa-edit"/>
+                                    </button>
+                                </td>
+                              </tr>
+                            </tbody>
+                         </table>
+                      </div>
+                   </div>
+                </div>
+        </div>
+</div>
+</template>
+
+<script>
+import axios from "axios";
+import ModalComponent from '@/components/Global/ModalComponent';
+import AddForm from './TypeStockForm';
+
+export default {
+    components: { ModalComponent, AddForm },
+    data() {
+        return{
+            modalActive: false,
+            search:'',
+            typeStocks :[]
+        }
+    },
+    mounted(){
+        this.fetchData()
+    },
+    computed:{
+        searchEvery(){
+            return this.typeStocks.filter(val=>val.includes(this.search))
+            }
+    },
+    methods:{
+        fetchData() {
+            axios.get(this.$store.state.baseUrl + "/typeStocks/")
+            .then(resp => {
+                this.typeStocks = resp.data
+            })
+            .catch(err => {
+                console.error(err)
+            })
+        },
+        deleteTStock(id) {
+            axios.delete(this.$store.state.baseUrl + "/typeStocks/" + id)
+            .then(resp => {
+                this.typeStocks = resp.data
+                this.fetchData()
+            })
+            .catch(err => {
+                console.error(err)
+            })
+            
+        },
+
+        editTypeStock(typeStock,id){
+        this.$store.state.IdEditTypStock=id
+        this.$store.state.typeStocks=typeStock
+        
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
