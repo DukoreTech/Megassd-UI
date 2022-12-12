2df82f6b124a9e5574154a2b90484ebf7e7ac4e9
CyberShriver <irakozep18.pi@gmail.com>
2022-12-12 10:26:23 +0000

    create produit list components

---
+++ src/components/Stock/Produit/ProduitList.vue
@@ -0,0 +1,115 @@
+<template>
+<div>
+        <div>        
+                <button class="btn btn-info m-5" @click="modalActive = true,$store.state.IdEditProduit=null">
+                    <font-awesome-icon icon="fa-solid fa-plus-circle" />
+                    Ajouter Produit
+                    </button>
+                <div class="m-3 float-end ">
+                        <input type="text" class="form-control"  v-model="search" placeholder="Search" @keypress.enter="searchEvery"/>
+                    </div>
+                <modal-component :modalActive="modalActive" @close="modalActive = !modalActive">
+                    <add-form @close="modalActive = !modalActive"/>
+                </modal-component>
+        </div>
+
+         <div class="container-fluid">
+            <div class="card  mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-mute">Liste des produits</h6>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered  table-striped table-hover text-center" id="dataTable" width="100%" cellspacing="0">
+                            <thead>
+                              <tr>                    
+                                <th scope="col">Id</th>
+                                <th scope="col">Nom</th>
+                                <th scope="col">caisse</th>
+                                <th scope="col">Unite de mesure</th>
+                                <th scope="col">Nombres de bouteilles</th>
+                                <th scope="col">Actions</th>
+                             </tr>
+                            </thead>
+                      
+                            <tbody>
+                               <tr v-for="produit in produits" :key="produit.id">
+                                <th scope="row">{{ produit.id }}</th>
+                                <td>{{ produit.nom }} </td>
+                                <td>{{ produit.caisse }} </td>
+                                <td>{{ produit.uniteDeMesure }} </td>
+                                <td>{{ produit.nbBouteille }} </td>
+                                <td>
+                                    <button class="btn btn-sm btn-default m-2"  @click="deleteProduit(produit.id)"><font-awesome-icon icon="fa-solid fa-trash"/>
+                                    </button>
+                                    <button class="btn btn-sm btn-default" @click="modalActive = true,editProduit(produit,produit.id)" >
+                                    <font-awesome-icon icon="fa-solid fa-edit"/>
+                                    </button>
+                                </td>
+                              </tr>
+                            </tbody>
+                         </table>
+                      </div>
+                   </div>
+                </div>
+        </div>
+</div>
+</template>
+
+<script>
+import axios from "axios";
+import ModalComponent from '@/components/Global/ModalComponent';
+import AddForm from './ProduitAddForm';
+
+export default {
+    components: { ModalComponent, AddForm },
+    data() {
+        return{
+            modalActive: false,
+            search:'',
+            produits : [ ]
+        }
+    },
+    mounted(){
+        this.fetchData()
+    },
+    computed:{
+        searchEvery(){
+            return this.produits.filter(val=>val.includes(this.search))
+            }
+    },
+    methods:{
+        fetchData() {
+            axios.get(this.$store.state.baseUrl + "/produits/")
+            .then(resp => {
+                this.produits = resp.data
+            })
+            .catch(err => {
+                console.error(err)
+            })
+        },
+        deleteProduit(id) {
+            axios.delete(this.$store.state.baseUrl + "/produits/" + id)
+            .then(resp => {
+                this.produits = resp.data
+                this.fetchData()
+            })
+            .catch(err => {
+                console.error(err)
+            })
+            
+        },
+
+        editProduit(produit,id){
+        this.$store.state.IdEditProduit=id
+        this.$store.state.produits=produit
+        
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
