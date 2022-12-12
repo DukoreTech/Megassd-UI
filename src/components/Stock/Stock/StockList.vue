e8ab11bd3b44c7355f6437e105c459b2b8851ba2
CyberShriver <irakozep18.pi@gmail.com>
2022-12-12 10:58:46 +0000

    create stock list comp

---
+++ src/components/Stock/Stock/StockList.vue
@@ -0,0 +1,111 @@
+<template>
+<div>
+        <div>        
+                <button class="btn btn-info m-5" @click="modalActive = true,$store.state.IdEditStock=null">
+                    <font-awesome-icon icon="fa-solid fa-plus-circle" />
+                    Ajouter stock
+                    </button>
+                <div class="m-3 float-end ">
+                        <input type="text" class="form-control"  v-model="search" placeholder="Search" @keypress.enter="searchEvery"/>
+                    </div>
+                <modal-component :modalActive="modalActive" @close="modalActive = !modalActive">
+                    <add-form @close="modalActive = !modalActive"/>
+                </modal-component>
+        </div>
+
+         <div class="container-fluid">
+            <div class="card  mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-mute">Liste des stock</h6>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered  table-striped table-hover text-center" id="dataTable" width="100%" cellspacing="0">
+                            <thead>
+                              <tr>                    
+                                <th scope="col">Id</th>
+                                <th scope="col">stock</th>
+                                <th scope="col">Quantite</th>
+                                <th scope="col">Actions</th>
+                             </tr>
+                            </thead>
+                      
+                            <tbody>
+                               <tr v-for="stock in stocks" :key="stock.id">
+                                <th scope="row">{{ stock.id }}</th>
+                                <td>{{ stock.produit }} </td>
+                                <td>{{ stock.Quantite }} </td>
+                                <td>
+                                    <button class="btn btn-sm btn-default m-2"  @click="deleteStock(stock.id)"><font-awesome-icon icon="fa-solid fa-trash"/>
+                                    </button>
+                                    <button class="btn btn-sm btn-default" @click="modalActive = true,editStock(stock,stock.id)" >
+                                    <font-awesome-icon icon="fa-solid fa-edit"/>
+                                    </button>
+                                </td>
+                              </tr>
+                            </tbody>
+                         </table>
+                      </div>
+                   </div>
+                </div>
+        </div>
+</div>
+</template>
+
+<script>
+import axios from "axios";
+import ModalComponent from '@/components/Global/ModalComponent';
+import AddForm from './StockAddForm';
+
+export default {
+    components: { ModalComponent, AddForm },
+    data() {
+        return{
+            modalActive: false,
+            search:'',
+            stocks : [ ]
+        }
+    },
+    mounted(){
+        this.fetchData()
+    },
+    computed:{
+        searchEvery(){
+            return this.stocks.filter(val=>val.includes(this.search))
+            }
+    },
+    methods:{
+        fetchData() {
+            axios.get(this.$store.state.baseUrl + "/stocks/")
+            .then(resp => {
+                this.stocks = resp.data
+            })
+            .catch(err => {
+                console.error(err)
+            })
+        },
+        deleteStock(id) {
+            axios.delete(this.$store.state.baseUrl + "/stocks/" + id)
+            .then(resp => {
+                this.stocks = resp.data
+                this.fetchData()
+            })
+            .catch(err => {
+                console.error(err)
+            })
+            
+        },
+
+        editStock(stock,id){
+        this.$store.state.IdEditStock=id
+        this.$store.state.stocks=stock
+        
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
